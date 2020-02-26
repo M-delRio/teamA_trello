9146d244ae0c78a94ac77ee2319fe91d59715864
Mike del Rio <mikevdelrio@gmail.com>
2020-02-25 21:03:29 -0500

    started front end, extracted board components to separate folder and created container to manage state a board component, Board.jsx includes static html however error encountered with rendering

---
+++ app/javascript/components/board/BoardContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import connect from "react-redux";
+import Board from "./Board";
+
+class BoardContainer extends Component {
+  render() {
+    return <Board />;
+  }
+}
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Board.destroy_all
 List.destroy_all
+Card.destroy_all
 
 board1 = Board.create title: "First board"
 
 list1 = List.create title: "first list", board: board1
+list2 = List.create title: "second list", board: board1
+
+card1 = Card.create title: "first card", list: list1
+card2 = Card.create title: "second card", list: list1
+card3 = Card.create title: "third card", list: list2
+card4 = Card.create title: "fourth card", list: list2
+card5 = Card.create title: "fifth card", list: list2
