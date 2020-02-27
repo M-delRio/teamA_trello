7a2538641a518a2530629630e9591fa760908ef5
Nathan Classen <hayesclassen@gmail.com>
2020-02-27 13:30:48 -0600

    added list update route

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -15,7 +15,17 @@ class Api::ListsController < ApplicationController
     end
 
     def update
+        @list = List.find(params[:id])
 
+        if @list.update(list_params)
+            return :update
+        else
+            @error = @list.errors.full_messages.join(', ')
+            render 'api/shared/error', status: :unprocessable_entity
+        end
+        rescue ActiveRecord::RecordNotFound
+            @error = "Invalid list id provided"
+            render 'api/shared/error', status: :unprocessable_entity
     end
 
     private
